667db12f3d94fd9569b10d53e5d9deb6ce11c998
root <root@localhost>
2024-09-20 16:10:32 +0300

    auto publish 1726837831854

---
+++ src/main.c
@@ -0,0 +1,12 @@
+/* kyu university 
+by the vc 
+to all new students*/
+#include <stdio.h> 
+int main(int ) {
+printf("students name\n");
+printf("admissions number\n");printf("original ID $Result slip\n");printf("or birth certificate \n");
+ printf("x ray film or cd\n");printf("medical reports and x-ray report \n"); printf("system registration \n");
+ printf("fees payment\n"); printf("biometric registration\n");printf("fileing documents \n");printf("reg completed successfully \n");
+ printf("Thanks 😊 😅 😂 😂 😂\n ");printf("you are now a student at kyu welcome🤝 ");
+  
+return 0;}
